f6e8da4d4e6bb625c47266053729103cfdf7d5c6
Ivan Yonchovski <yyoncho@gmail.com>
2019-08-10 22:41:41 +0300

    Implement start-simple

--- start-emacs-vanilla.sh
+++ start-emacs-vanilla.sh
@@ -5,9 +5,8 @@ docker run -ti --rm -v $('pwd'):/mnt/workspace \
        -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /etc/machine-id:/etc/machine-id:ro \
-       -v ~/.emacs.d-docker/:/root/.emacs.d \
-       -v $(pwd)/.spacemacs-minimal:/root/.spacemacs \
        -v $(pwd)/demo-projects/:/Projects \
+       -v $(pwd)/emacs.d/:/root/.emacs.d \
        -v $(pwd)/local/share/fonts:/root/.local/share/fonts/ \
        -v ~/.m2/:/root/.m2 \
        -e DISPLAY=$DISPLAY \
