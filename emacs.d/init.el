efea24afa68cfb37cc7638d84a1ea13fb8979b3a
Ivan Yonchovski <yyoncho@gmail.com>
2019-08-22 22:06:10 +0300

    Start vanilla

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,6 @@
-(setq package-selected-packages '(lsp-mode lsp-ui company-lsp yasnippet lsp-java lsp-python-ms lsp-haskell helm-lsp lsp-treemacs dap-mode lsp-origami helm-lsp))
+(setq package-selected-packages
+      '(lsp-mode lsp-ui company-lsp yasnippet lsp-java lsp-python-ms lsp-haskell helm-lsp lsp-treemacs dap-mode lsp-origami helm-lsp
+                 rust-mode php-mode typescript-mode kotlin-mode))
 
                                         ; list the repositories containing them
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
@@ -11,10 +13,15 @@
 
 (package-install-selected-packages)
 
-(with-eval-after-load 'cc-mode
-  (define-key java-mode-map (kbd ";") nil)
-  (define-key java-mode-map (kbd "(") nil))
+(with-eval-after-load 'lsp-mode
+  (require 'yasnippet)
+  (require 'lsp-java))
 
 (add-hook 'prog-mode-hook 'lsp)
 
+;; non lsp related stuff
 (ido-mode)
+
+(with-eval-after-load 'cc-mode
+  (define-key java-mode-map (kbd ";") nil)
+  (define-key java-mode-map (kbd "(") nil))
