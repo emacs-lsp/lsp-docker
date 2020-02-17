c30a5c0083c08b55b45f93dd0426970b4e29e478
Reza Nikoopour <rnikoopour@gmail.com>
2020-02-16 23:12:10 -0800

    Added checks for darwin

--- start-emacs-vanilla.sh
+++ start-emacs-vanilla.sh
@@ -1,8 +1,15 @@
+IS_DARWIN=`uname | grep -i darwin`
+LOCAL_TIME_VOLUME=`[ -z "$IS_DARWIN" ] &&  echo "-v /etc/localtime:/etc/localtime:ro"`
+SYSTEM_BUS_SOCKET_VOLUME=`[ -z "$IS_DARWIN" ] && echo "-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket"`
+
+echo $SYSTEM_BUS_SOCKET_VOLUME
+
+
 docker run -ti --rm -v $('pwd'):/mnt/workspace \
-       -v /etc/localtime:/etc/localtime:ro \
+       $LOCAL_TIME_VOLUME \
        -v ~/.ssh/id_rsa:${UHOME}/.ssh/id_rsa:ro \
        -v ~/.gnupg:${UHOME}/.gnupg \
-       -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
+       $SYSTEM_BUS_SOCKET_VOLUME \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /etc/machine-id:/etc/machine-id:ro \
        -v $(pwd)/demo-projects/:/Projects \
@@ -11,4 +18,4 @@ docker run -ti --rm -v $('pwd'):/mnt/workspace \
        -v ~/.m2/:/root/.m2 \
        -e DISPLAY=$DISPLAY \
        -e TZ=UA \
-       yyoncho/lsp-emacs-docker:latest emacs $*
+       emacslsp/lsp-docker-full:latest emacs $*
