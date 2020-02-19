fda47141262892ac85bd5ad0af4d37eb097b974b
Reza Nikoopour <rnikoopour@gmail.com>
2020-02-18 21:24:27 -0800

    Python works

--- demo-projects/Python/hello-world/hello.py
+++ demo-projects/Python/hello-world/hello.py
@@ -1,4 +1,11 @@
-# foo bar
-message = "Hello world"
+class Greeter():
+    def __init__(self, greeting):
+        self.greeting = greeting
 
-print message
+    def say_hello(self):
+        print(self.greeting + " world")
+
+
+if __name__ == "__main__":
+    a_greeter = Greeter("Hola")
+    a_greeter.say_hello()
