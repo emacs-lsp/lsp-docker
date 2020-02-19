994a145c8ddef8ec3321d9b5c31dd7d5b6586f09
Reza Nikoopour <rnikoopour@gmail.com>
2020-02-18 21:24:27 -0800

    Python works

--- start-emacs-vanilla.sh
+++ start-emacs-vanilla.sh
@@ -1,21 +1,28 @@
 IS_DARWIN=`uname | grep -i darwin`
+EMACS_D_VOLUME=${EMACS_D_VOLUME:-"-v $(pwd)/emacs.d/:/root/.emacs.d"}
+PROJECTS_VOLUME=${PROJECTS_VOLUME:-"-v `pwd`/demo-projects/:/Projects"}
 
-# The following mounts are protected in Darwin and can't be used
-LOCAL_TIME_VOLUME=`[ -z "$IS_DARWIN" ] &&  echo "-v /etc/localtime:/etc/localtime:ro"`
-SYSTEM_BUS_SOCKET_VOLUME=`[ -z "$IS_DARWIN" ] && echo "-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket"`
+if [ -z "$IS_DARWIN" ]; then
+    # The following mounts are protected in Darwin and can't be used
+    LOCAL_FONTS_VOLUME="-v `pwd`/local/share/fonts:/root/.local/share/fonts/"
+    LOCAL_TIME_VOLUME="-v /etc/localtime:/etc/localtime:ro"
+    MACHINE_ID_VOLUME="-v /etc/machine-id:/etc/machine-id:ro"
+    SYSTEM_BUS_SOCKET_VOLUME="-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket"
+    # Not used
+    X11_VOLUME="-v /tmp/.X11-unix:/tmp/.X11-unix"
+fi
+TIME_ZONE=${TZ:-Europe/Minsk}
+ # -v ~/.ssh/id_rsa:${UHOME}/.ssh/id_rsa:ro \
+ # -v ~/.gnupg:${UHOME}/.gnupg \
 
 docker run -ti --rm -v $('pwd'):/mnt/workspace \
+       $EMACS_D_VOLUME \
+       $PROJECTS_VOLUME \
        $LOCAL_TIME_VOLUME \
-       -v ~/.ssh/id_rsa:${UHOME}/.ssh/id_rsa:ro \
-       -v ~/.gnupg:${UHOME}/.gnupg \
+       $MACHINE_ID_VOLUME \
        $SYSTEM_BUS_SOCKET_VOLUME \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v /etc/machine-id:/etc/machine-id:ro \
-       -v $(pwd)/demo-projects/:/Projects \
-       -v $(pwd)/emacs.d/:/root/.emacs.d \
-       -v $(pwd)/local/share/fonts:/root/.local/share/fonts/ \
-       -v ~/.m2/:/root/.m2 \
+       $X11_VOLUME \
        -e DISPLAY=$DISPLAY \
-       -e TZ=UA \
+       -e TZ=$TIME_ZONE \
        emacslsp/lsp-docker-full:latest emacs $*
 #       yyoncho/lsp-emacs-docker emacs $*
