3f6de506a9464e2b3ca6b55ad9a750854ed0297f
Reza Nikoopour <rnikoopour@gmail.com>
2020-02-19 22:29:49 -0800

    redid c++ demo

--- demo-projects/CPP/hello-world/hello.cpp
+++ demo-projects/CPP/hello-world/hello.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
+#include <string>
 
-int main() { std::cout; }
+class Greeter {
+  std::string greeting;
+
+public:
+  Greeter(std::string greeting) { this->greeting = greeting; }
+  void SayHello() { std::cout << this->greeting << " world!"; }
+};
+
+int main() {
+  auto greeter = Greeter("Salam");
+  greeter.SayHello();
+
+  return 0;
+}
